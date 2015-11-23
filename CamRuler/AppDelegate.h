fc0af5e0bfcce6c1f70cd6ba4034fab059f696da
seandonato <seanvincentdonato@gmail.com>
2015-11-23 17:05:16 -0500

    Initial Commit

---
+++ CamRuler/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CamRuler
+//
+//  Created by Sean Donato on 11/23/15.
+//  Copyright © 2015 Sean Donato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CamRuler/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CamRuler
+//
+//  Created by Sean Donato on 11/23/15.
+//  Copyright © 2015 Sean Donato. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
